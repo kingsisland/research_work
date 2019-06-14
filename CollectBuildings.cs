be0bb1318978f080172cccbf5b07d9949eea1f8b
Karthik Vedantam <31345581+kingsisland@users.noreply.github.com>
2019-06-13 17:50:16 -0700

    Initial commit

---
+++ CollectBuildings.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using Wrld;
+using Wrld.Space;
+using Wrld.Resources.Buildings;
+using UnityEngine;
+
+public class CollectBuildings: MonoBehaviour
+{
+
+    public float radius;
+    public Vector3 centre;
+    private LatLong buildingLocation;
+    public GameObject sphere;
+    public GameObject square;
+    public float trans;
+    private Color Color;
+    void Start()
+    {     
+     
+       
+        Color = sphere.GetComponent<Renderer>().material.color;
+
+     
+
+
+
+
+    }
+    private void Update()
+    {
+        Color.a = trans;
+        sphere.transform.position = square.transform.position;
+        sphere.transform.localScale = new Vector3(radius, radius, radius);
+        //sphere.transform.localScale = new Vector3(radius, radius, radius);
+       
+
+    }
+
+    //private void OnEnable()
+    //{
+    //    StartCoroutine(Example());
+    //}
+
+    
+
+    //IEnumerator Example()
+    //{
+    //    Api.Instance.CameraApi.MoveTo(buildingLocation, distanceFromInterest: 500, headingDegrees: 0, tiltDegrees: 45);
+
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(4.0f);
+
+    //        BuildingHighlight.Create(
+    //            new BuildingHighlightOptions()
+    //                .HighlightBuildingAtLocation(buildingLocation)
+    //                .BuildingInformationReceivedHandler(this.OnBuildingInformationReceived)
+    //                .InformationOnly()
+    //        );
+    //    }
+    //}
+
+    //void OnBuildingInformationReceived(BuildingHighlight highlight)
+    //{
+    //    if (highlight.IsDiscarded())
+    //    {
+    //        Debug.Log(string.Format("No building information was received"));
+    //        return;
+    //    }
+
+    //    var buildingInformation = highlight.GetBuildingInformation();
+
+    //    var boxAnchor = Instantiate(boxPrefab) as GameObject;
+    //    boxAnchor.GetComponent<GeographicTransform>().SetPosition(buildingInformation.BuildingDimensions.Centroid);
+
+    //    var box = boxAnchor.transform.GetChild(0);
+    //    box.localPosition = Vector3.up * (float)buildingInformation.BuildingDimensions.TopAltitude;
+    //    Destroy(boxAnchor, 2.0f);
+
+    //    Debug.Log(string.Format("Building information received: {0}", buildingInformation.ToJson()));
+
+    //    highlight.Discard();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    StopAllCoroutines();
+    //}
+}
---
+++ TestScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using Wrld;
+using Wrld.Space;
+using Wrld.Resources.Buildings;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class TestScript : MonoBehaviour
+{
+    public float radius;
+    
+    public GameObject sphere;
+
+    public float partsXZ = 5f;
+    public float partsInY = 5f;
+    private float x, y, z;
+    public bool allSet ;
+    private  HashSet<Vector3> BuildingsList = new HashSet<Vector3>();
+    public float ConeAngle = 60f;
+    public float ConeSpread = 90f;
+    public float RayDurationTime = 10f;
+
+ 
+
+    void Start()
+    {
+        allSet = false;
+        
+    }
+
+
+     void Update()
+    {   
+        if(allSet == true)
+        {
+            JuiceUp();
+            allSet = false;
+        }
+
+    }
+
+    private void JuiceUp()
+    {
+        
+        float incrementTheta = ConeSpread / partsXZ;
+        float incrementGamma = (90f - ConeAngle ) / partsInY;
+
+       /* Debug.Log(incrementTheta);
+        Debug.Log(incrementGamma);*/
+
+        float r = 1f;
+        float ThetaInDegrees = 0f;
+        for( ; ThetaInDegrees <= ConeSpread; ThetaInDegrees += incrementTheta )
+        {   
+           
+            float GammaInDegrees = 90f;
+
+             Debug.Log("Theta: " + ThetaInDegrees);
+            
+
+           for (; GammaInDegrees >= ConeAngle; GammaInDegrees -= incrementTheta )
+            {
+               
+
+
+                /*Debug.Log("Gamma : " + GammaInDegrees);
+                Debug.Log("ConeAngle : " + ConeAngle);*/
+
+                x = r * Mathf.Sin(GammaInDegrees * Mathf.Deg2Rad) * Mathf.Cos(ThetaInDegrees * Mathf.Deg2Rad);
+                y = r * Mathf.Cos( GammaInDegrees * Mathf.Deg2Rad);
+                z = r * Mathf.Sin(GammaInDegrees * Mathf.Deg2Rad) * Mathf.Sin(ThetaInDegrees * Mathf.Deg2Rad);
+
+                FireSomeRays(x, y, z);           // Fires Rays in all directions above ground
+                FireSomeRays(-x, y, z);
+                FireSomeRays(x, y, -z);
+                FireSomeRays(-x, y, -z);
+
+                
+            }
+
+            
+
+        }
+
+        foreach (var item in BuildingsList)
+        {
+            Debug.Log(item.x + "  " + item.y + "  " + item.z);
+        }
+
+        
+    }
+
+    private void FireSomeRays(float x, float y, float z)    
+    {
+        RaycastHit hit;
+      
+        if (Physics.Raycast(sphere.transform.position,new Vector3(x, y, z), out hit, radius))
+        {
+            
+            Debug.DrawRay(sphere.transform.position, -sphere.transform.position + hit.point, Color.yellow, 60f);
+            AddBuildingToList(hit);
+        }
+       else
+            Debug.DrawRay(sphere.transform.position, new Vector3(x, y, z) * radius, Color.red, RayDurationTime);
+
+
+    }
+
+
+    private void AddBuildingToList( RaycastHit hit)    //Get the hit information and adds it to a HashSet
+    {
+         BuildingsList.Add(hit.point);
+    }
+    
+
+}
